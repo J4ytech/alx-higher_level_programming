2914b1b19c52251adf96e6f893f244103c832696
Joseph Agaba <agabsokwori@gmail.com>
2026-05-19 00:08:01 +0100

    Implement to_json_string function with pycodestyle compliance

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module: 3-to_json_string
+This module defines a function that returns
+the JSON representation of a Python object.
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object (string).
+
+    Args:
+        my_obj: The Python object to serialize.
+
+    Returns:
+        str: JSON string representation of the object.
+    """
+    return json.dumps(my_obj)
