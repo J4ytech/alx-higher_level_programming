3d2c0a3dd9711cf4193d3bb7b3095c1b80ef6662
Joseph Agaba <agabsokwori@gmail.com>
2026-05-18 21:29:09 +0100

    fixed misspelt function

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,7 +4,7 @@ This module contains the is_same_class function
 """
 
 
-def is_kind_of_class(obj, a_class):
+def is_same_class(obj, a_class):
     """
     Returns True if obj is exactly an instance of a_class; otherwise False.
     """
