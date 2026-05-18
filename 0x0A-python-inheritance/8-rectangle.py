5e2c56e4e5036bfb36081b35f0e03e445b341d15
Joseph Agaba <agabsokwori@gmail.com>
2026-05-18 22:43:38 +0100

    Add Rectangle class with input validation

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module contains the Rectangle class that inherits from BaseGeometry
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A rectangle class that inherits from BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Initializes the rectangle with validated width and height"""
+
+        self.integer_validator("width", width)
+        self.__width = width
+
+        self.integer_validator("height", height)
+        self.__height = height
